d99007d1949d1dcc128fc3c2301f4a5d34e625ca
dpenkin@gmail.com <dpenkin@gmail.com@63c72f72-3c94-11de-a9fe-ad1e22fc3a8a>
2009-05-13 15:03:05 +0000

    Code format /hmm... idea's client is not the best one/

--- util/src/ru/ifmo/team/util/PropReader.java
+++ util/src/ru/ifmo/team/util/PropReader.java
@@ -4,8 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * User: Daniel Penkin
