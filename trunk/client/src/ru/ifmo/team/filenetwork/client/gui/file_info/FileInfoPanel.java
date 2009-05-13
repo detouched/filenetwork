5014c29c271bd8f8f7938d13166e83acd0aaa484
Andrey.Borisenko <Andrey.Borisenko@63c72f72-3c94-11de-a9fe-ad1e22fc3a8a>
2009-05-13 15:21:32 +0000

    Some new GUI concepts, also neutral (from Danny's paths) properties files.

---
+++ trunk/client/src/ru/ifmo/team/filenetwork/client/gui/file_info/FileInfoPanel.java
@@ -0,0 +1,53 @@
+package ru.ifmo.team.filenetwork.client.gui.file_info;
+
+import ru.ifmo.team.filenetwork.SharedFile;
+import sun.awt.shell.ShellFolder;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class FileInfoPanel extends JPanel {
+    private JList foreignList;
+    private JLabel fileIconLabel = new JLabel("<no file selected>");
+
+
+    public FileInfoPanel(JList foreignList) {
+
+        this.foreignList = foreignList;
+        this.add(fileIconLabel);
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);    //To change body of overridden methods use File | Settings | File Templates.
+
+        if (!foreignList.isSelectionEmpty()) {
+            try {
+                //Create a temporary file with the specified extension
+                SharedFile sharedFile = (SharedFile) foreignList.getSelectedValue();
+                int extensionIndex = sharedFile.getName().lastIndexOf(".");
+                final String fileExtension = sharedFile.getName().substring(extensionIndex);
+
+                File file = File.createTempFile("icon", fileExtension);
+                ShellFolder shellFolder = ShellFolder.getShellFolder(file);
+                ImageIcon icon = new ImageIcon(shellFolder.getIcon(true));
+
+                //this.getGraphics().drawImage(icon,0,0,null);
+
+                fileIconLabel.setText(sharedFile.getName());
+                fileIconLabel.setIcon(icon);
+                //fileIconLabel.repaint();
+
+//Delete the temporary file
+                file.delete();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            fileIconLabel.setText("<no file selected>");
+        }
+    }
+}
